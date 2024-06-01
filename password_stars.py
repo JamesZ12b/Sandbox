5f92ccd572a10182cc443635ae55de67a88e7ba9
Jzkx666 <zzhankai2@gmail.com>
2024-06-02 06:39:07 +0800

    password star

--- password_stars.py
+++ password_stars.py
@@ -1,3 +1,8 @@
-a=input("Enter a string: ")
-if a=="Pythonista":
-    print("**********")
+MIN_LENGTH = 6
+password = input("Enter the password: ")
+length = len(password)
+while (length < MIN_LENGTH):
+    print("Invaild length")
+    password = input("Enter the password: ")
+    length = len(password)
+print('*' * length)
